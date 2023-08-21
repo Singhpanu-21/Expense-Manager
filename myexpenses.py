ec3819433195385e2d759366db5d0a2e945e42cf
Singhpanu-21 <harshitpanu16@gmail.com>
2023-08-21 16:35:43 +0530

    first commit

---
+++ myexpenses.py
@@ -0,0 +1,121 @@
+from tkinter import *
+from tkinter import ttk
+import sqlite3 as db
+
+from tkcalendar import DateEntry
+
+
+def init():
+    connectionObjn = db.connect("expenseTrackerss.db")
+    curr = connectionObjn.cursor()
+    query = '''
+    create table if not exists expenses (
+        date string,
+        name string,
+        title string,
+        expense number
+        )
+    '''
+    curr.execute(query)
+    connectionObjn.commit()
+
+
+def submitexpense():
+    values = [dateEntry.get(), Name.get(), Title.get(), Expense.get()]
+    print(values)
+    Etable.insert('', 'end', values=values)
+
+    connectionObjn = db.connect("expenseTrackerss.db")
+    curr = connectionObjn.cursor()
+    query = '''
+    INSERT INTO expenses VALUES 
+    (?, ?, ?, ?)
+    '''
+    curr.execute(query, (dateEntry.get(), Name.get(), Title.get(), Expense.get()))
+    connectionObjn.commit()
+
+
+def viewexpense():
+    connectionObjn = db.connect("expenseTrackerss.db")
+    curr = connectionObjn.cursor()
+    query = '''
+     select * from expenses
+    '''
+    total = '''
+    select sum(expense) from expenses
+    '''
+    curr.execute(query)
+    rows = curr.fetchall()
+    curr.execute(total)
+    amount = curr.fetchall()[0]
+    print(rows)
+    print(amount)
+    l = Label(root, text="Date\t\t  Name\t\t  Title\t\t  Expense", font=('arial', 15, 'bold'), bg="Orange", fg="white")
+    l.grid(row=4, column=30, padx=7, pady=7)
+
+    st = ""
+    for i in rows:
+        for j in i:
+            st += solve(str(j))
+        st += '\n'
+    st += "\n"
+    if rows:
+        st += solve("TOTAL-") +  " "*80+ solve(str(amount[0]))
+    print(st)
+    l = Label(root, text=st, font=('arial', 12))
+    l.grid(row=5, column=30, padx=7, pady=7)
+
+def solve(n):
+    x = len(n)
+    mid = (40-x)
+    return " "*(mid//2) + n + " "*(mid - (mid//2))
+
+
+
+init()
+root = Tk()
+root.title("ProjectGurukul Expense tracker")
+root.geometry('800x600')
+
+dateLabel = Label(root, text="Date", font=('arial', 15, 'bold'), bg="DodgerBlue2", fg="white", width=12)
+dateLabel.grid(row=0, column=0, padx=7, pady=7)
+
+dateEntry = DateEntry(root, width=12, font=('arial', 15, 'bold'))
+dateEntry.grid(row=0, column=1, padx=7, pady=7)
+
+Name = StringVar()
+nameLabel = Label(root, text="Name", font=('arial', 15, 'bold'), bg="DodgerBlue2", fg="white", width=12)
+nameLabel.grid(row=1, column=0, padx=7, pady=7)
+
+NameEntry = Entry(root, textvariable=Name, font=('arial', 15, 'bold'))
+NameEntry.grid(row=1, column=1, padx=7, pady=7)
+
+Title = StringVar()
+titleLabel = Label(root, text="Title", font=('arial', 15, 'bold'), bg="DodgerBlue2", fg="white", width=12)
+titleLabel.grid(row=2, column=0, padx=7, pady=7)
+
+titleEntry = Entry(root, textvariable=Title, font=('arial', 15, 'bold'))
+titleEntry.grid(row=2, column=1, padx=7, pady=7)
+
+Expense = IntVar()
+expenseLabel = Label(root, text="Expense", font=('arial', 15, 'bold'), bg="DodgerBlue2", fg="white", width=12)
+expenseLabel.grid(row=3, column=0, padx=7, pady=7)
+
+expenseEntry = Entry(root, textvariable=Expense, font=('arial', 15, 'bold'))
+expenseEntry.grid(row=3, column=1, padx=7, pady=7)
+
+submitbtn = Button(root, command=submitexpense, text="Submit", font=('arial', 15, 'bold'), bg="DodgerBlue2", fg="white",
+                   width=12)
+submitbtn.grid(row=4, column=0, padx=13, pady=13)
+
+viewtn = Button(root, command=viewexpense, text="View expenses", font=('arial', 15, 'bold'), bg="DodgerBlue2",
+                fg="white", width=12)
+viewtn.grid(row=4, column=1, padx=13, pady=13)
+
+# all saved expenses--------------
+Elist = ['Date', 'Name', 'Title', 'Expense']
+Etable = ttk.Treeview(root, column=Elist, show='headings', height=10)
+for c in Elist:
+    Etable.heading(c, text=c.title())
+Etable.grid(row=5, column=0, padx=7, pady=7, columnspan=3)
+mainloop()
